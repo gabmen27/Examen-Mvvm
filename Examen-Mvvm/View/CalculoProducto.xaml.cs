37c17a20d86d8651f38d848849196d80c2126905
gabmen27 <gabrieljesus680@gmail.com>
2025-03-26 20:32:10 -0600

    Se Termino de crear la funcion calcular, y se agregaron 3 entris mas en en el front end que dice que tipo de descuento se aplico y cuanto se paga ya con el descuento incluido

--- Examen-Mvvm/View/CalculoProducto.xaml.cs
+++ Examen-Mvvm/View/CalculoProducto.xaml.cs
@@ -1,9 +1,18 @@
+using Examen_Mvvm.ViewModels;
+
 namespace Examen_Mvvm.View;
 
 public partial class CalculoProducto : ContentPage
 {
+	CalculoDescuentoProducto viewModel;
 	public CalculoProducto()
 	{
 		InitializeComponent();
-	}
+
+		viewModel = new CalculoDescuentoProducto();
+
+		BindingContext = viewModel;
+
+
+    }
 }
--- Examen-Mvvm/ViewModels/CalculoDescuentoProducto.cs
+++ Examen-Mvvm/ViewModels/CalculoDescuentoProducto.cs
@@ -8,13 +8,33 @@ namespace Examen_Mvvm.ViewModels
     {
         [ObservableProperty]
         private double precio1;
-        
+
 
         [ObservableProperty]
         private double precio2;
 
         [ObservableProperty]
         private double precio3;
+
+        [ObservableProperty]
+        double descuento1;
+        
+        [ObservableProperty]
+        double descuento2;
+
+        [ObservableProperty]
+        double descuento3;
+
+        [ObservableProperty]
+        string txtDescuento1;
+
+        [ObservableProperty]
+        string txtDescuento2;
+       
+
+        [ObservableProperty]
+        string txtDescuento3;
+
 
         private void Alerta(string Titulo, string Mensaje)
         {
@@ -24,36 +44,109 @@ namespace Examen_Mvvm.ViewModels
         [RelayCommand]
         private void Calcular()
         {
-
-            double descuento1;
-            double descuento2;
-            double descuento3;
             try
             {
-                if (Precio1 <= 0)
+                if (Precio1 < 0)
                 {
-                    Alerta("ADVERTENCIA", "Ingrese un monto válido");
+                    Alerta("ADVERTENCIA", "Ingrese un Precio Valido");
                 }
-                else if (Precio2 <= 0)
+                else if (Precio2 < 0)
                 {
-                    Alerta("ADVERTENCIA", "Ingrese una tasa anual válida");
+                    Alerta("ADVERTENCIA", "Ingrese un Precio Valido");
                 }
-                else if (Precio3 <= 0)
+                else if (Precio3 < 0)
                 {
-                    Alerta("ADVERTENCIA", "Ingrese la cantidad de años válido");
+                    Alerta("ADVERTENCIA", "Ingrese un Precio Valido");
                 }
-                else {
+                else
+                {
 
-                    if (Precio1 >= 10000.00) {
-                        descuento1 = Precio1* 0.3;
+                    if (Precio1 >= 10000.00)
+                    {
+                        TxtDescuento1 = "Descuento del 30%, total a Pagar: ";
+                        Descuento1 = Precio1-(Precio1 * 0.3);
                     }
-                
+                    if (Precio2 >= 10000.00)
+                    {
+                        TxtDescuento2 = "Descuento del 30%, total a Pagar:";
+                        Descuento2 = Precio2 - (Precio2 * 0.3);
+                    }
+                    if (Precio3 >= 10000.00)
+                    {
+                        TxtDescuento3 = "Descuento del 30%, total a Pagar:";
+                        Descuento3 = Precio3 - (Precio3 * 0.3);
+                    }
+
+                    if (Precio1 >= 5000 && Precio1 <= 9999.99) {
+                        TxtDescuento1 = "Descuento del 20%, total a Pagar:";
+                        Descuento1 = Precio1 - (Precio1 * 0.2);
+                    }
+                    if (Precio2 >= 5000 && Precio2 <= 9999.99)
+                    {
+                        TxtDescuento2 = "Descuento del 20%, total a Pagar:";
+                        Descuento2 = Precio2 - (Precio2 * 0.2);
+                    }
+                    if (Precio1 >= 5000 && Precio1 <= 9999.99)
+                    {
+                        TxtDescuento3 = "Descuento del 20%, total a Pagar:";
+                        Descuento3 = Precio3 - (Precio3 * 0.2);
+                    }
+
+                    if (Precio1 >= 1000 && Precio1 <= 4999.99)
+                    {
+                        TxtDescuento1 = "Descuento del 10%, total a Pagar:";
+                        Descuento1 = Precio1 - (Precio1 * 0.1);
+                    }
+                    if (Precio2 >= 1000 && Precio2 <= 4999.99)
+                    {
+                        TxtDescuento2 = "Descuento del 10%, total a Pagar:";
+                        Descuento2 = Precio1 - (Precio1 * 0.1);
+                    }
+                    if (Precio3 >= 1000 && Precio1 <= 4999.99)
+                    {
+                        TxtDescuento3 = "Descuento del 10%, total a Pagar:";
+                        Descuento3 = Precio1 - (Precio1 * 0.1);
+                    }
+                    
+                    if(Precio1 >= 0 && Precio1<=999.99){
+                        TxtDescuento1 = "No Aplica Descuento, total a Pagar:";
+                        Descuento1 = Precio1;
+                    }
+                    if (Precio2 >= 0 && Precio2 <= 999.99) {
+                        TxtDescuento2 = "No Aplica Descuento, total a Pagar:";
+                        Descuento2 = Precio2;
+                    }
+                    if (Precio3 >= 0 && Precio3 <= 999.99)
+                    {
+                        TxtDescuento3 = "No Aplica Descuento, total a Pagar:";
+                        Descuento3 = Precio3;
+                    }
+
+
                 }
-                
+
             }
             catch (Exception ex)
             {
                 Alerta("ERROR", ex.Message);
             }
         }
+
+
+        [RelayCommand]
+        private void Limpiar() {
+            Precio1 = 0;
+            Precio2 = 0;
+            Precio3 = 0;
+
+            Descuento1 = 0;
+            Descuento2 = 0;
+            Descuento3 = 0;
+
+            TxtDescuento1 = "";
+            TxtDescuento2 = "";
+            TxtDescuento3 = "";
+
+        }
     }
+}
